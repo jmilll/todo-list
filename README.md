1babc778d8550b7ebd9f4678bbbf81608197648f
jmilll <jmillgames@gmail.com>
2021-01-05 17:25:41 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+
+# To Do List Webapp
+A 
+
+[Check it](https://jmilll.github.io/todo-list/)
+
+## Example
+
+![Default Page](assets/home.png)
+
+![Contact Page](assets/contact.png)
+
+[Check it](https://jmilll.github.io/todo-list/)
+
+## Features
+
+* Webpack Bundle
+
+## Motivation
+
+The purpose of this 
+
+## Technologies Used
+
+* Webpack
+* JavaScript
+* HTML / CSS
+* Flexbox
+
+## Acknowledgements
+
+This project was inspired by [The Odin Project](https://www.theodinproject.com/courses/javascript/lessons/todo-list) online learning curriculum.
+
+## License
+
+MIT
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import './normalize.css';
+import './style.css';
+import loadHome from './module/home';
+
+console.log('new site');
+//loadHeader();
+
+function clear() {
+    const container = document.getElementById('content');
+    let conLength = container.children.length;
+
+    // remove each element other than header
+    while (conLength > 1) {
+        container.removeChild(container.children[1])
+        conLength--;
+    } 
+}
+
+// Header buttons
+const logoBtn = document.querySelector('.logo');
+logoBtn.onclick = () => {
+    clear();
+    loadHome();
+    loadFooter();
+    initFooterBtn()
+};
+
