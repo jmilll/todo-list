b9e735fb38e972561f5589db38d5e7f416cfe762
jmilll <jmillgames@gmail.com>
2021-01-11 17:48:41 -0500

    added dom elements and listeners for task editing and adding

--- src/index.js
+++ src/index.js
@@ -8,18 +8,65 @@
 
 //import { Collapse } from '../node_modules/bootstrap/js/dist/collapse';
 
+//import collapse from '../node_modules/bootstrap/js/dist/collapse';
+//import '../node_modules/bootstrap/js/dist/collapse';
+
 console.log('new todo list 2');
 //loadHeader();
 
-//DOM CONTROL
+// ---------- DOM CONTROL ---------- 
 /*
-var buttons = document.querySelectorAll('.btn')
-buttons.forEach(function (button) {
-  var button = new bootstrap.Button(button)
-  button.toggle()
-})
 */
+//
+// EDIT PROJECT INFO BUTTONS
+const editProjectBtn = document.getElementById('edit-project-btn');
+editProjectBtn.addEventListener('click', () => {
 
+    const p = document.querySelectorAll('.task-heading');
+    p.forEach(x => x.classList.toggle('visually-hidden'));
+});
+
+const saveEditBtn = document.querySelector('#save-edit-btn');
+saveEditBtn.addEventListener('click', () => {
+
+   // if (!task-name.value) return;
+
+   const p = document.querySelectorAll('.task-heading');
+   p.forEach(x => x.classList.toggle('visually-hidden'));
+
+    console.log('save edit');
+});
+
+const cancelEditBtn = document.querySelector('#cancel-edit-btn');
+cancelEditBtn.addEventListener('click', () => {
+
+   // if (!task-name.value) return;
+
+   const p = document.querySelectorAll('.task-heading');
+   p.forEach(x => x.classList.toggle('visually-hidden'));
+
+    console.log('cancel edit');
+});
+
+
+//
+// TASK DOM EVENTS
+const taskCheckBoxes2 = document.querySelectorAll('div.todo input.form-check-input');
+taskCheckBoxes2.forEach(box => box.addEventListener('click', () => {
+    //box.classList.toggle('checked');
+    box.parentElement.querySelector('p').classList.toggle('complete');
+    console.log('checkme');
+}))
+
+const taskCheckBoxes = document.querySelectorAll('div.todo div.checkbox');
+taskCheckBoxes.forEach(box => box.addEventListener('click', () => {
+    box.classList.toggle('checked');
+    box.parentElement.querySelector('p').classList.toggle('complete');
+    console.log('checkme');
+}))
+
+//
+// ADD TASK FORM BUTTONS
 const newTaskBtn = document.querySelector('#add-task-btn');
 newTaskBtn.addEventListener('click', () => {
     newTaskBtn.classList.add('visually-hidden');
@@ -34,7 +81,7 @@ cancelTaskBtn.addEventListener('click', () => {
 
 const submitTaskBtn = document.querySelector('#submit-task-btn');
 submitTaskBtn.addEventListener('click', () => {
-    
+
    // if (!task-name.value) return;
 
     newTaskBtn.classList.remove('visually-hidden');
@@ -43,7 +90,10 @@ submitTaskBtn.addEventListener('click', () => {
     console.log('submit form');
 });
 
-
+const optionsBtn = document.querySelector('#options-btn');
+optionsBtn.addEventListener('click', () => {
+    document.querySelector('.form-features').classList.toggle('visually-hidden')
+});
 
 /*
 
