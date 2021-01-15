74e3b1cd376661dc5a054bc6e3a7519d8b471454
jmilll <jmillgames@gmail.com>
2021-01-15 17:17:36 -0500

    all projects load, more tweeks needed

--- src/index.js
+++ src/index.js
@@ -17,102 +17,139 @@ console.log('new todo list 2');
 //import renderTasks from './module/task-creation';
 //********** ---------- PROJECTS  ----------********** /**/
 
-let allProjects = []
+
+
+
+//let allProjects = []
+
+let allProjects = [
+    {name: 'Default',
+    description: 'Big time default',
+    todo: [
+        {taskName: "default todo", date: "2021-01-19", priority: "3", checked: false},
+    ],
+    active: true,
+    },
+    {name: 'Test',
+    description: 'Testing out the project bud',
+    todo: [
+        {taskName: "first todo", date: "2021-01-19", priority: "1", checked: false},
+        {taskName: "aa", date: "2021-01-19", priority: "1", checked: false},
+        {taskName: "bb", date: "2021-01-19", priority: "2", checked: true},
+        {taskName: "cc", date: "2021-01-19", priority: "3", checked: false},
+    ],
+    active: false,
+    },
+]
+
+
+
+
+
+
 
 const newProject = (name, description) => {
-    allProjects.push({name,
+    allProjects.push({
+    name,
     description,
     todo: [],
     active: true,
     })
 
-    console.log(allProjects);
 }
 //newProject('Test', 'Testing out the project')
 
-const addProjectToDOM = (name, description) => {
-    const taskContainer = document.querySelector('.tasks-container');
 
-    const project = document.createElement('div');
-    project.classList.add('task');
-
-    const heading = document.createElement('div');
-    heading.classList.add('task-heading');
-        const headingIfo = document.createElement('div');
-        headingIfo.classList.add('project-info');
-            const title = document.createElement('h1');
-            title.classList.add('display-1');
-            //content from project
-            title.textContent = name;
-            const desc = document.createElement('p');
-            desc.classList.add('task-description');
-            //content from project
-            desc.textContent = description;
-        const editInfoBtn = document.createElement('button');
-        editInfoBtn.type = 'button';
-        editInfoBtn.classList.add('btn');
-        editInfoBtn.classList.add('btn-outline-dark');
-        editInfoBtn.classList.add('no-border');
-        editInfoBtn.setAttribute('id', 'edit-project-btn');
-            const iconEdit = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
-            <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"></path>
-            </svg>`;
-
-
-    const headingEdit = document.createElement('div');
-    headingEdit.classList.add('task-heading');
-    headingEdit.classList.add('edit');
-    headingEdit.classList.add('visually-hidden');
-        const projectTitleEdit = document.createElement('input');
-            projectTitleEdit.type = 'text';
-            projectTitleEdit.classList.add('form-control');
-            projectTitleEdit.setAttribute('id', 'project-title');
-            //content from project
-            projectTitleEdit.placeholder = name;
-            projectTitleEdit.required = true;
-        const projectDescriptionEdit = document.createElement('input');
-            projectDescriptionEdit.type = 'text';
-            projectDescriptionEdit.classList.add('form-control');
-            projectDescriptionEdit.setAttribute('id', 'project-description');
-            //content from project
-            projectDescriptionEdit.placeholder = description;
-        const formBtnContainer = document.createElement('div');
-
-    const divider = document.createElement('div');
-    divider.classList.add('divider');
-
-    const todoContain = document.createElement('div');
-    todoContain.classList.add('todo-container');
-
-    const addTaskBtn = document.createElement('button');
-
-
-    const taskSubmit = document.createElement('form');
+//when his save on new project button
+function addNewProject() {
+    let name = getAddProjectValues().name;
+    let desc = getAddProjectValues().desc;
 
 
 
-    taskContainer.appendChild(project);
-    project.appendChild(heading);
-        heading.appendChild(headingIfo);
-            headingIfo.appendChild(title);
-            headingIfo.appendChild(desc);
-        heading.appendChild(editInfoBtn);
-            editInfoBtn.insertAdjacentHTML("beforeend", iconEdit);
-    project.appendChild(headingEdit);
-        headingEdit.appendChild(projectTitleEdit);
-        headingEdit.appendChild(projectDescriptionEdit);
-        headingEdit.appendChild(formBtnContainer);
-            formBtnContainer.appendChild();
-            formBtnContainer.appendChild();
-    project.appendChild(divider);
+    newProject(name, desc);
+    addProjectToDOM(name);
+    loadProject(name, desc);
+    
+    console.log(allProjects);
+}
+
+
+function clearActiveProjects(){
+    allProjects.forEach(project => project.active = false);
 }
 
 
 
-            /*const iconEdit = document.createElement('svg');
-            iconEdit.xmlns = 'http://www.w3.org/2000/svg';
-            iconEdit.setAttribute('width', '16')
-            iconEdit.setAttribute('height', '16')*/
+//put project into task area
+const loadProject = (name, description) => {
+    const projectName = document.querySelector('div.project-info h1.display-1')
+    const projectDescription = document.querySelector('div.project-info p.task-description')
+
+    projectName.textContent = name;
+    projectDescription.textContent = description;
+
+
+    const projectNameEdit = document.querySelector('#project-title')
+    const projectDescriptionEdit = document.querySelector('#project-description')
+
+    projectNameEdit.placeholder = name;
+    projectDescriptionEdit.placeholder = description;
+
+    //renderTasks()
+}
+
+function searchActive () {
+    /*
+    allProjects.forEach(project => {
+        for (const [key, value] of Object.entries(project)) {
+            console.log(`${key}: ${value}`)};
+    })
+    */
+    for (let i = 0; i < allProjects.length; i++) {
+        const element = allProjects[i];
+        if (element.active === true) {
+            return  i;
+        }
+    }
+}
+
+function renderProjects() {
+    allProjects.forEach(project => {
+        addProjectToDOM(project.name)
+    })
+
+}
+
+const getAddProjectValues = () => {
+    const name = document.querySelector('#new-title').value
+    const desc = document.querySelector('#new-description').value
+    
+    return {name, desc}
+}
+
+
+//addProjectToDOM('Clean');
+function addProjectToDOM(name) {
+    const categories = document.querySelector('.categories');
+    let x = categories.childElementCount;
+
+    const projectDiv = document.createElement('div');
+    projectDiv.setAttribute('data-value', x);
+    projectDiv.classList.add('item');
+    //variable text content
+    projectDiv.textContent = name;
+
+
+    categories.appendChild(projectDiv);
+}
+
+function clearProjectsDOM() {
+    const categories = document.querySelector('.categories');
+    while (categories.firstChild) {
+        categories.removeChild(categories.firstChild);
+    }
+}
 
 //********** ---------- TASK CREATION ----------********** /**/
 
@@ -122,7 +159,7 @@ const taskList = [
     {taskName: "cc", date: "2021-01-19", priority: "3", checked: false}
 ];
 
-renderTasks()
+
 
 const todoItem = (taskName, date, priority, checked) => {
     return { 
@@ -196,7 +233,7 @@ const addTaskForm = (() => {
     }
 })();
 
-function createNewTask () {
+function createNewTask() {
     let taskName = addTaskForm.getTask().taskName;
     let date = addTaskForm.getTask().date;
     let priority = addTaskForm.getTask().priority;
@@ -208,17 +245,26 @@ function createNewTask () {
     console.log(taskList);
 }
 
-function clearTasks() {
+function clearTasksDOM() {
     const tasks = document.querySelectorAll('.todo');
     tasks.forEach(t => document.querySelector('.todo-container').removeChild(t));
 }
 
+function renderTasks(list) {
+    list.forEach(task => {
+        addTaskToDOM(task.taskName, task.date, task.priority, task.checked)
+    });
+}
+/*
 function renderTasks() {
     taskList.forEach(task => {
         addTaskToDOM(task.taskName, task.date, task.priority, task.checked)
     });
 }
+*/
 
+//renderTasks()
+renderProjects()
 
 
 // **********---------- DOM CONTROL ----------********** /**/
@@ -226,7 +272,35 @@ function renderTasks() {
 
 
 //
-// CATEGORY ITEM CONTROL **********
+// PROJECT ITEM CONTROL **********
+//DYNAMIC
+const cat = document.querySelector('.categories');
+cat.addEventListener('click', (e) => {
+    if (!e.target) { return; }
+    if (e.target.matches('.item')) {
+        const categoryItem = document.querySelectorAll('.item');
+        for (let i = 0; i<categoryItem.length; i++) {
+            categoryItem[i].classList.remove('select')
+        }
+        e.target.classList.add('select');
+
+        let dataVal = e.target.dataset.value;
+        let specificProject = allProjects[dataVal];
+
+        clearActiveProjects()
+        specificProject.active = true;
+
+
+        clearTasksDOM()
+        renderTasks(specificProject.todo);
+
+        //console.log(specificProject.todo)
+    }
+
+})
+
+/*
+//moved to dynamic
 const categoryItem = document.querySelectorAll('.item');
 categoryItem.forEach(item => item.addEventListener('click', () => {
     for (let i = 0; i<categoryItem.length; i++) {
@@ -235,7 +309,7 @@ categoryItem.forEach(item => item.addEventListener('click', () => {
     //console.log(categoryItem);
     item.classList.add('select');
 }))
-
+*/
 const newProjectBtn = document.getElementById('new-project-btn');
 newProjectBtn.addEventListener('click', () => {
     document.querySelector('.sub-heading-edit').classList.toggle('visually-hidden');
@@ -258,36 +332,8 @@ cancelNewBtn.addEventListener('click', () => {
     console.log('cancel-new-project');
 });
 
-//
-// EDIT PROJECT INFO BUTTONS **********
-const editProjectBtn = document.getElementById('edit-project-btn');
-editProjectBtn.addEventListener('click', () => {
 
-    const p = document.querySelectorAll('.task-heading');
-    p.forEach(x => x.classList.toggle('visually-hidden'));
-});
 
-const saveEditBtn = document.querySelector('#save-edit-btn');
-saveEditBtn.addEventListener('click', () => {
-
-   // if (!task-name.value) return;
-
-   const p = document.querySelectorAll('.task-heading');
-   p.forEach(x => x.classList.toggle('visually-hidden'));
-
-    console.log('save edit');
-});
-
-const cancelEditBtn = document.querySelector('#cancel-edit-btn');
-cancelEditBtn.addEventListener('click', () => {
-
-   // if (!task-name.value) return;
-
-   const p = document.querySelectorAll('.task-heading');
-   p.forEach(x => x.classList.toggle('visually-hidden'));
-
-    console.log('cancel edit');
-});
 
 
 //
@@ -335,12 +381,73 @@ taskCheckBoxes.forEach(box => box.addEventListener('click', () => {
 
 
 
+// EDIT PROJECT  INFO BUTTONS **********
+const editProjectBtn = document.getElementById('edit-project-btn');
+editProjectBtn.addEventListener('click', () => {
+
+    const p = document.querySelectorAll('.task-heading');
+    p.forEach(x => x.classList.toggle('visually-hidden'));
+});
+
+const saveEditBtn = document.querySelector('#save-edit-btn');
+saveEditBtn.addEventListener('click', () => {
+
+   // if (!task-name.value) return;
+
+   const p = document.querySelectorAll('.task-heading');
+   p.forEach(x => x.classList.toggle('visually-hidden'));
+
+    console.log('save edit');
+});
+
+const cancelEditBtn = document.querySelector('#cancel-edit-btn');
+cancelEditBtn.addEventListener('click', () => {
+
+   // if (!task-name.value) return;
+
+   const p = document.querySelectorAll('.task-heading');
+   p.forEach(x => x.classList.toggle('visually-hidden'));
+
+    console.log('cancel edit');
+});
 
 
 
-
-//
 // ADD TASK FORM BUTTONS **********
+const newTaskBtn = document.querySelector('#add-task-btn');
+newTaskBtn.onclick = () => {
+    newTaskBtn.classList.add('visually-hidden');
+    document.querySelector('.task-submit').classList.remove('visually-hidden')
+};
+
+const cancelTaskBtn = document.querySelector('#cancel-btn');
+cancelTaskBtn.addEventListener('click', () => {
+    newTaskBtn.classList.remove('visually-hidden');
+    document.querySelector('.task-submit').classList.add('visually-hidden')
+});
+
+const submitTaskBtn = document.querySelector('#submit-task-btn');
+submitTaskBtn.addEventListener('click', (e) => {
+    //stop empty task add
+    if (!addTaskForm.getTask().taskName) return;
+    //prevent page refresh on submit
+    e.preventDefault();
+
+    newTaskBtn.classList.remove('visually-hidden');
+    document.querySelector('.task-submit').classList.add('visually-hidden')
+    createNewTask();
+
+    //reset form bc prevented default operations
+    document.querySelector(".task-submit").reset();
+});
+
+const optionsBtn = document.querySelector('#options-btn');
+optionsBtn.addEventListener('click', () => {
+    document.querySelector('.form-features').classList.toggle('visually-hidden')
+});
+
+
+/*
 const newTaskBtn = document.querySelector('#add-task-btn');
 newTaskBtn.addEventListener('click', () => {
     newTaskBtn.classList.add('visually-hidden');
